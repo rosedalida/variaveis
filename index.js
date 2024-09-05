5d5236194bef6ad972196a5614b16294dc6e7010
ROSE DALEE MARCELLUS <35544475-rosemarcellus@users.noreply.replit.com>
2024-09-05 01:12:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let anoATUAL=2024
+console.log(anoATUAL);
+console.log(anoAtual);
+
+anoAtual=2025;
+
+console.log(`òlá, eu sou o ${ Rose Dalee Marcellus} ,eu tenho ${18} anos ,moro em $ {são josé dos pinhais} , quero )
